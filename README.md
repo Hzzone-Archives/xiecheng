2ceaed83f1d5cd0778c907afcf87bbffb4521575
Hzzone <Hzzone@outlook.com>
2018-02-05 23:14:00 +0800

    complete

--- README.md
+++ README.md
@@ -14,9 +14,26 @@ wish_down_threshold = 1000
 ```
 则当机票最低价低于1000，`qq_email_address`发送邮件给`inform_email_address`。    
 每一小时刷新一次，机票信息变化频率很低，不需要过快，可以自己手动设置，但是不建议太快。
-
+### Requirements
+```
+PyExecJS
+requests
+```
+### 注意事项
+携程爬取机票时，如果直接get这样的url:
+```
+http://flights.ctrip.com/booking/CTU-SHA-day-1.html?DDate1=2018-03-13
+```
+并不会得到机票信息，但是如果你get这个url:
+```
+http://flights.ctrip.com/domesticsearch/search/SearchFirstRouteFlights?DCity1=CTU&ACity1=SHA&SearchType=S&DDate1=2018-03-13&IsNearAirportRecommond=0&LogToken=980342546776496ebab5631bf4a30f85&rk=7.201366593865179225816&CK=E2CB39252256436934ECE11B8F9951F9&r=0.53186630183055359500419
+```
+在浏览器中抓包，可以获得返回的数据，在`return_data.json`中。 
+但是当你直接get第二个url，返回的数据并不完全甚至为空。
+因此需要执行一段js代码。   
+最简单的方式是使用`phantomjs`，这里使用pyexecjs执行js代码。
 ### 如何使用
 需要配置config.py，具体如何修改请看注释
-* 发送邮件
+* 发送邮件  
 发送邮件使用了qq邮箱，请打开qq邮箱->设置->账户->POP3/IMAP/SMTP/Exchange/CardDAV/CalDAV服务->开启服务->温馨提示，生成授权码     
 如果你要使用自定义邮箱，请重写`sendEmail`函数。
--- xiecheng.py
+++ xiecheng.py
@@ -8,6 +8,29 @@ import smtplib
 from email.mime.text import MIMEText
 import time
 
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+handler = logging.FileHandler("xiecheng.log")
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+
+logger.addHandler(handler)
+logger.addHandler(console)
+
+logger.info('''
+    start station: %s
+    arrive station: %s
+    wish date: %s
+    qq email address: %s
+    inform email address: %s
+    refreshing interval: %ss
+    ''' % (config.start_station, config.arrive_station, " ".join(config.wish_date_list), 
+        config.qq_email_address, config.inform_email_address, config.interval))
 
 '''
 subject: 主题
@@ -26,8 +49,8 @@ def sendEmail(subject, msg, _to):
         s.login(config.qq_email_address, config.qq_email_code)
         s.sendmail(config.qq_email_address, _to, msg.as_string())
         s.quit()
-    except smtplib.SMTPException:
-        print("Falied,%s"%e)
+    except smtplib.SMTPException as e:
+        logger.error("Falied,%s"%e)
 
 headers1={
     "Host":"flights.ctrip.com",
@@ -49,6 +72,7 @@ headers2={
   "Pragma":"no-cache",
   "Connection":"keep-alive"
 }
+
 #封装函数
 def getData(city1,city2,date):
     #获取加密js
@@ -58,7 +82,8 @@ def getData(city1,city2,date):
     try:
         cookie=req1.headers["Set-Cookie"]
     except:
-        print(str(count1)+" "+city1+"-"+city2+":"+"获取cookie失败\n")
+        # print(str(count1)+" "+city1+"-"+city2+":"+"获取cookie失败\n")
+        logger.error(str(count1)+" "+city1+"-"+city2+":"+"获取cookie失败\n")
     return_data=req1.text
     js_match=re.findall('var fn=(.*?)var jsonCallback',return_data)
     js=''
@@ -75,20 +100,19 @@ def getData(city1,city2,date):
         js=js.replace(', !0);',');')
         js=js.replace('t.send(null);','')
         js="function ajaxRequest(n,t){"+js
-        url_match=re.findall('var url = "(.*?)";',return_data)
+        url_match=re.findall('var url = "(.*?)";', return_data)
         # print(url_match)
         if(url_match):
             indexUrl="http:"+url_match[0]
-            print(indexUrl)
         else:
-            print("未匹配到对应url信息")
+            logger.error("未匹配到对应url信息")
         para_match=re.findall('ajaxRequest\(url(.*?)\'\);',return_data)
         if(para_match):
             para=para_match[0]
         else:
-            print("未匹配到对应参数信息")
+            logger.error("未匹配到对应url信息")
         js="function getUrl(){var i='';var lastUrl='';var url='"+indexUrl+"';"+js+"ajaxRequest(url"+para+"');return lastUrl;}"
-
+        
         #执行加密js得到加密后的url
         ctx=execjs.compile(js)
         dataUrl=(ctx.call("getUrl"))
@@ -119,9 +143,9 @@ def getData(city1,city2,date):
                 info_list.append(slist)
             return sorted(info_list, key=lambda x:x[-1])
         except:
-          print("获取数据失败")
+            logger.error("获取数据失败")
     else:
-        print("未匹配到对应js代码")
+        logger.error("未匹配到对应js代码")
 
 def main():
 	#获取所有城市
@@ -132,18 +156,23 @@ def main():
         city1_code = all_city[config.start_station]
         city2_code = all_city[config.arrive_station]
     except:
-        print("没有该城市编码")
+        logger.error("没有该城市编码")
     while True:
         for wish_date in config.wish_date_list:
             info_list = getData(city1_code, city2_code, wish_date)
-            print(info_list)
-            loweset_plane = info_list[0]
+            # print(info_list)
+            try:
+                loweset_plane = info_list[0]
+            except:
+                logger.error('爬取到的数据为空')
+                exit(1)
+            logger.info('<%s->%s %s> current loweset price: %s' % (config.start_station, config.arrive_station, wish_date, loweset_plane[-1]))
             if loweset_plane[-1] <= config.wish_down_threshold:
                 send_content = "航班 %s\n<%s %s> 到 <%s %s>\n出发时间: %s\n到达时间: %s\n现价: %s\n" % (
                         loweset_plane[0], loweset_plane[1], loweset_plane[2], loweset_plane[3], loweset_plane[4],
                         loweset_plane[5], loweset_plane[6], loweset_plane[7])
                 sendEmail('携程机票心愿最低价达成', send_content, config.inform_email_address)
-                print(send_content)
+                logger.info(send_content)
             time.sleep(10)
         time.sleep(config.interval)
 
